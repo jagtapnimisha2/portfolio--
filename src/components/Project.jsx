550f78c3a3274b0ec00ff40bc0dd5229f0fa1738
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2025-01-18 03:48:03 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,14 +2,14 @@ import React from "react";
 // import i1 from "../components/image/i1.png"
 
 import imgport from "../components/image/imgport.png"
-import imag1 from "../components/image/imag1.png"
-import imag2 from "../components/image/imag2.png"
-import imag3 from "../components/image/imag3.png"
-import imag4 from "../components/image/imag4.png"
-import imag5 from "../components/image/imag5.png"
-import imag6 from "../components/image/imag6.png"
-import imag7 from "../components/image/imag7.png"
-import imag8 from "../components/image/imag8.png"
+// import imag1 from "../components/image/imag1.png"
+// import imag2 from "../components/image/imag2.png"
+// import imag3 from "../components/image/imag3.png"
+// import imag4 from "../components/image/imag4.png"
+// import imag5 from "../components/image/imag5.png"
+// import imag6 from "../components/image/imag6.png"
+// import imag7 from "../components/image/imag7.png"
+// import imag8 from "../components/image/imag8.png"
 import imag9 from "../components/image/imag9.png"
 import imag10 from "../components/image/imag10.png"
 import imag11 from "../components/image/imag11.png"
@@ -18,7 +18,7 @@ import imag13 from "../components/image/imag13.png"
 import imag14 from "../components/image/imag14.png"
 import imag15 from "../components/image/imag15.png"
 import imag16 from "../components/image/imag16.png"
-import imag17 from "../components/image/imag17.png"
+// import imag17 from "../components/image/imag17.png"
 import imag18 from "../components/image/imag18.jpeg"
 import imag19 from "../components/image/imag19.jpeg"
 const Project=()=>{
@@ -26,57 +26,75 @@ const Project=()=>{
         {
             id:0,
             src:imgport,
-            githublink:"https://github.com/jagtapnimisha2/portfolio--",
-            link:"https://main--harmonious-stroopwafel-b4b833.netlify.app/" 
+            githublink:"https://github.com/jagtapnimisha2/Nimisha-portfolio?tab=readme-ov-file",
+            link:"https://nimisha-portfollio.netlify.app/" 
         },
         {
-            id:1,
-            src:imag1,
-            githublink:"https://github.com/jagtapnimisha2/Ineuron_assignment-01",
-            link:"https://jagtapnimisha2.github.io/Ineuron_assignment-01/"
+            id:18,
+            src:imag18,
+            githublink:"https://github.com/jagtapnimisha2/Searching-cookies-recipe-list?tab=readme-ov-a",
+            link:"https://searching-cookies-recipe.netlify.app/"
         },
         {
-            id:2,
-            src:imag2,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-02",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-02/"
+            id:19,
+            src:imag19,
+            githublink:"https://github.com/jagtapnimisha2/imdb-main1",
+            link:"https://clone-moviess.netlify.app/"
         },
         {
-            id:3,
-            src:imag3,  
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-03",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-03/"
+            id:16,
+            src:imag16,
+            githublink:"https://github.com/jagtapnimisha2/LinkedIn-Clone---React-Project---kyiq4msruph7",
+            link:"https://glowing-tapioca-25d26b.netlify.app/"
         },
-        {
-            id:4,
-            src:imag4,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-04",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-04/"
-        },
-        {
-            id:5,
-            src:imag5,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-05",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-05/"
-        },
-        {
-            id:6,
-            src:imag6,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-06",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-06/"
-        },
-        {
-            id:7,
-            src:imag7,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-07",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-07/"
-        },
-        {
-            id:8,
-            src:imag8,
-            githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-08",
-            link:"https://jagtapnimisha2.github.io/ineuron-assignment-08/"
-        },
+        // {
+        //     id:1,
+        //     src:imag1,
+        //     githublink:"https://github.com/jagtapnimisha2/Ineuron_assignment-01",
+        //     link:"https://jagtapnimisha2.github.io/Ineuron_assignment-01/"
+        // },
+        // {
+        //     id:2,
+        //     src:imag2,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-02",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-02/"
+        // },
+        // {
+        //     id:3,
+        //     src:imag3,  
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-03",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-03/"
+        // },
+        // {
+        //     id:4,
+        //     src:imag4,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-04",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-04/"
+        // },
+        // {
+        //     id:5,
+        //     src:imag5,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-05",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-05/"
+        // },
+        // {
+        //     id:6,
+        //     src:imag6,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-06",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-06/"
+        // },
+        // {
+        //     id:7,
+        //     src:imag7,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-07",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-07/"
+        // },
+        // {
+        //     id:8,
+        //     src:imag8,
+        //     githublink:"https://github.com/jagtapnimisha2/ineuron-assignment-08",
+        //     link:"https://jagtapnimisha2.github.io/ineuron-assignment-08/"
+        // },
         {
             id:9,
             src:imag9,
@@ -119,30 +137,14 @@ const Project=()=>{
             githublink:"https://github.com/jagtapnimisha2/ineuron-assignment--15",
             link:"https://jagtapnimisha2.github.io/ineuron-assignment--15/"
         },
-        {
-            id:16,
-            src:imag16,
-            githublink:"https://github.com/jagtapnimisha2/LinkedIn-Clone---React-Project---kyiq4msruph7",
-            link:"https://glowing-tapioca-25d26b.netlify.app/"
-        },
-        {
-            id:17,
-            src:imag17,
-            githublink:"https://github.com/jagtapnimisha2/Garbage_Bin__Major_Project",
-            link:"https://smartgarbage-f4f3b.web.app"
-        },
-        {
-            id:18,
-            src:imag18,
-            githublink:"https://github.com/jagtapnimisha2/Searching-cookies-recipe-list?tab=readme-ov-a",
-            link:"https://searching-cookies-recipe.netlify.app/"
-        },
-        {
-            id:19,
-            src:imag19,
-            githublink:"https://github.com/jagtapnimisha2/imdb-main1",
-            link:"https://clone-moviess.netlify.app/"
-        }
+        
+        // {
+        //     id:17,
+        //     src:imag17,
+        //     githublink:"https://github.com/jagtapnimisha2/Garbage_Bin__Major_Project",
+        //     link:"https://smartgarbage-f4f3b.web.app"
+        // },
+       
     ]
     return(
  <div
