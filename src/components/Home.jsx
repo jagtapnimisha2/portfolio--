a4d1d2109ad74ee66a2e24cf8504860d4ab51e63
jagtapnimisha2 <120504401+jagtapnimisha2@users.noreply.github.com>
2025-01-18 02:27:25 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,55 @@
 // rafce
-import React from 'react'
+import React from 'react';
 import heroImage from '../assets/heroImage.png';
 import { RiArrowRightSFill } from "react-icons/ri";
 import { Link } from 'react-scroll';
+import './Home.css'
+
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        {/* <img src={heroImage} alt='' /> */}
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-2xl sm:text-5xl font-bold text-white pt-28 max-h-px max-w-md '>I'm a Front End Developer</h2>
-                <p className='text-gray-500 pt-28 max-w-md'> 
-                    I have 3 month of experience building and desgining 
-                    software.
-                    Currently, I love to work on web application using
-                    technologies like
-                    Html,Css,React Js,and Tailwind.
-                </p>
-                <div>
-            <Link to='Project' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-red-500 to-red-300 cursor-pointer'>
-            Project
-                <span className='group-hover:rotate-90 duration-300'>
-                    <RiArrowRightSFill size={28} className="ml-1"/> 
-                </span>
+    <div
+      name="home"
+      className="h-full w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      {/* <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center  h-full  px-4 md:flex-row"> */}
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-auto min-h-screen px-4 md:flex-row md:h-auto lg:h-full ">
+        {/* Text Section */}
+        <div className="flex flex-col justify-center h-full text-center md:text-left mt-20">
+          <h2 className="text-3xl sm:text-5xl font-bold text-white mt-10  md:mt-20 expr ">
+            I'm a Front End Developer
+          </h2>
+          <p className="text-gray-500 py-6 max-w-md mx-auto md:mx-0">
+            Frontend (UI) Developer with 1+ year of experience across multiple
+            companies. Skilled in developing responsive websites and dynamic web
+            applications. Proficient in graphic design and creating user
+            interfaces.
+          </p>
+          <div className="flex justify-center md:justify-start">
+            <Link
+              to="Project"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 flex items-center rounded-md bg-gradient-to-r from-red-500 to-red-300 cursor-pointer"
+            >
+              Projects
+              <span className="group-hover:rotate-90 duration-300">
+                <RiArrowRightSFill size={28} className="ml-1" />
+              </span>
             </Link>
+          </div>
         </div>
+
+        {/* Image Section */}
+        <div className=" home-img flex justify-center mt-10 md:mt-0 hero-img">
+          <img
+            src={heroImage}
+            alt="My Profile"
+            className="rounded-2xl w-3/4 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+          />
+        </div>
+      </div>
     </div>
-    <div>
-        <img src={heroImage} alt='my profile' className='rounded-2xl max-h-64 max-w-screen-md mx-6 sm:ml-6 sm:max-h-80'/>
-        {/* mx-auto w-2/3 md:w-full */}
-    </div>
-   </div>
-   </div>
-    
-  )
-}
+  );
+};
 
 export default Home;
